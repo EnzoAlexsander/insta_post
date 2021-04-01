5b8d1003201c5295a2297595e35451b5a4cd1d68
EnzoAlexsander <enzoalexsander9@gmail.com>
2021-04-01 20:27:09 -0300

    [UPDATE] Movendo main pra dentro do electron

---
+++ electron/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+
+function App() {
+    const win = require('./createWindow.js')
+}
+
+app.whenReady().then(App);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
